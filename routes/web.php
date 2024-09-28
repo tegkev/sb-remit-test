da9c1b3f67c1ad879d5eae091cf71382e0e1950b
Kevin Teguia <teguiakevin@gmail.com>
2024-09-28 01:23:56 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'version' => '1.0.0',
+        'resources' => ['blogs']
+    ];
 });
